106cf6cae3226a8d2aa075aa68c7173563d8879d
Philippe <philippe@igor-brevnjovski.net>
2022-02-16 10:59:57 +0100

    Add state machine classes

--- src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
+++ src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
@@ -14,8 +14,12 @@ import org.springframework.jms.support.converter.MessageType;
 @Configuration
 public class JmsConfig {
 	
-	public static final String MY_QUEUE = "my-hello-world";
-	public static final String MY_SEND_RCV_QUEUE = "my-sendandreceive-queue";
+    public static final String VALIDATE_ORDER_QUEUE = "validate-order";
+    public static final String VALIDATE_ORDER_RESPONSE_QUEUE = "validate-order-response";
+    public static final String ALLOCATE_ORDER_QUEUE = "allocate-order";
+    public static final String ALLOCATE_ORDER_RESPONSE_QUEUE = "allocate-order-response";
+    public static final String ALLOCATE_FAILURE_QUEUE = "allocation-failure";
+    public static final String DEALLOCATE_ORDER_QUEUE = "deallocate-order" ;
 
     @Bean
     public MessageConverter messageConverter(){
