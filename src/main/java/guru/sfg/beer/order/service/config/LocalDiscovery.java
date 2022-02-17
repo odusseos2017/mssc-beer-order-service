aa98a1079b9b6b9ea736e4167ea97b2dd9783afd
Philippe <philippe@igor-brevnjovski.net>
2022-02-17 18:19:11 +0100

    Before Feign

--- src/main/java/guru/sfg/beer/order/service/config/LocalDiscovery.java
+++ src/main/java/guru/sfg/beer/order/service/config/LocalDiscovery.java
@@ -4,9 +4,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-/**
- * Created by jt on 3/7/20.
- */
 @Profile("local-discovery")
 @EnableDiscoveryClient
 @Configuration
